fb16460774faade9d645f00cec2fd564d9c8184e
oliviaarden <mac99@SRMAC02.local>
2016-03-15 13:28:39 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-var $heading prepend(<h1> "Bellow ground veggies" </h1>);
+var $newH1 = $('<H1>');
+var $belowVeggies = $('<ul>');
+var $aboveH1 = $('<h1>');
+var $aboveVeggies = $('<ul>');
+
+$newH1.html('Below ground veggies');
+$('body').prepend($newH1);
+
+$('body').append($belowVeggies);
+
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
+
+$('body').append($aboveVeggies);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $belowVeggies.append($(this));
+  } else {
+    $aboveVeggies.append($(this));
+  }
+
+});
